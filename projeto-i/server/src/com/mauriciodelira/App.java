1205b27b4b84d0446d464836484e888183237ba7
Mauricio de Lira <mauriciodlira@gmail.com>
2018-05-10 01:41:23 -0300

    Fix identação, resposta e logs

--- projeto-i/server/src/com/mauriciodelira/App.java
+++ projeto-i/server/src/com/mauriciodelira/App.java
@@ -2,38 +2,35 @@ package com.mauriciodelira;
 
 import com.mauriciodelira.server.LocalServer;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class App {
 
-    /**
-     * First parameter should be the default port
-     */
-    public static void main(String[] args) {
+  /**
+   * First parameter should be the default port
+   */
+  public static void main(String[] args) {
 
-        int port = 54054;
-        if(!args[0].isEmpty())
-            port = Integer.parseInt(args[0]);
+    int port = 54054;
+    if(!args[0].isEmpty())
+      port = Integer.parseInt(args[0]);
 
-        try {
-            ServerSocket server = new ServerSocket(port);
-            System.out.println("SERVER -- Servidor ouvindo em: " + server.getInetAddress() + ":" +server.getLocalPort());
+    try {
+      ServerSocket server = new ServerSocket(port);
+      System.out.println("SERVER -- Servidor ouvindo em: " + server.getInetAddress() + ":" +server.getLocalPort());
 
-            do {
-//                Aceita uma conexão com o cliente e abre uma nova thread
-                Thread t = new Thread(new LocalServer(server.accept()));
-                t.start();
+      do {
+//        Aceita uma conexão com o cliente e abre uma nova thread
+        Thread t = new Thread(new LocalServer(server.accept()));
+        t.start();
 
-            } while (!server.isClosed());
+      } while (!server.isClosed());
 
-        } catch (IOException e) {
-
-        }
-
+    } catch (IOException e) {
 
     }
+
+
+  }
 }
--- projeto-i/server/src/com/mauriciodelira/server/LocalServer.java
+++ projeto-i/server/src/com/mauriciodelira/server/LocalServer.java
@@ -1,81 +1,89 @@
 package com.mauriciodelira.server;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.Socket;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
-public class LocalServer implements Runnable{
-    final private String baseDir = System.getProperty("user.dir").concat("/public/");
-    private Socket client;
+public class LocalServer implements Runnable {
+  final private String baseDir = System.getProperty("user.dir").concat("/public/");
+  private Socket client;
 
-    public LocalServer(Socket clientConn) {
-        this.client = clientConn;
-    }
+  public LocalServer(Socket clientConn) {
+    this.client = clientConn;
+  }
 
-    public void run() {
-        try{
-            System.out.println("SERVER -- Aceitou uma conexão. Cliente em: "+client.getInetAddress()+":"+client.getPort());
+  public void run() {
+    try {
+      DataInputStream reader = new DataInputStream(client.getInputStream());
+      DataOutputStream writer = new DataOutputStream(client.getOutputStream());
 
-            DataInputStream reader = new DataInputStream(client.getInputStream());
-            DataOutputStream writer = new DataOutputStream(client.getOutputStream());
+      String fileName = reader.readUTF();
 
-            String fileName = reader.readUTF();
+      System.out.println("SERVER #" + Thread.currentThread().getName() + " -- Aceitou uma conexão. Cliente em: " +
+        client.getInetAddress() + ":" + client.getPort() + " busca por: " + fileName);
 
-            writer.writeUTF(getFileStatus(fileName));
-            writer.writeUTF(readFile(fileName));
+      writer.writeUTF(getFileStatus(fileName));
+      writer.writeUTF(readFile(fileName));
 
-            if(!client.isClosed())
-                client.close();
+      if (!client.isClosed())
+        client.close();
 
-            System.out.println("SERVER -- Conexão encerrada com o cliente em: "+
-                    client.getInetAddress()+":"+client.getPort());
+      System.out.println("SERVER #" + Thread.currentThread().getName() + " -- Conexão encerrada com o cliente em: " +
+        client.getInetAddress() + ":" + client.getPort());
 
-        } catch (IOException e) {
-
-        }
-
-    }
-
-    private String file2String(File file, String charset) throws IOException {
-        String message = "";
-        LineIterator it = FileUtils.lineIterator(file, charset);
-        while(it.hasNext()) {
-            message = message.concat(it.next());
-        }
-
-        return message;
-    }
-
-    private String getFileStatus(String filename) {
-        File file = new File(baseDir.concat(filename));
-
-        if(!file.exists()) return "400";
-        else return "200";
+    } catch (IOException e) {
 
     }
 
-    private String readFile(String filename) throws IOException {
-        File file = new File(baseDir.concat(filename));
+  }
 
-        String content;
-        String[] parts = filename.split("\\.");
-        if(file.exists() && parts.length > 1){
-            if(parts[1].equalsIgnoreCase("html") || parts[1].equalsIgnoreCase("txt")) {
-                content = file2String(file, "UTF-8");
-            } else {
-                content = "Arquivo não suportado";
-            }
-        } else {
-            content = "Arquivo [" + filename + "] não encontrado.";
-        }
+  private String file2String(File file, String charset) throws IOException {
+    String message = "";
 
-        return (
-            String.format("\nHTTP/1.1 %s\n" +
-                "// Body content of the file\n" +
-                "%s", getFileStatus(filename), content)
-        );
+    if (charset.isEmpty())
+      charset = "UTF-8";
+
+    LineIterator it = FileUtils.lineIterator(file, charset);
+    while (it.hasNext()) {
+      message = message.concat(it.next() + "\n");
     }
+
+    return message;
+  }
+
+  private String getFileStatus(String filename) {
+    File file = new File(baseDir.concat(filename));
+
+    if (!file.exists()) return "400";
+    else return "200";
+
+  }
+
+  private String readFile(String filename) throws IOException {
+    File file = new File(baseDir.concat(filename));
+
+    String content;
+    String[] parts = filename.split("\\.");
+    if (file.exists() && parts.length > 1) {
+      if (parts[1].equalsIgnoreCase("html") || parts[1].equalsIgnoreCase("txt")) {
+        content = file2String(file, "UTF-8");
+      } else {
+        content = "Arquivo não suportado";
+      }
+    } else {
+      content = "Arquivo [" + filename + "] não encontrado.";
+    }
+
+    return (
+      String.format("\nHTTP/1.1 %s\n" +
+        "// Body content of the file\n" +
+        "%s", getFileStatus(filename), content)
+    );
+  }
 
 }
